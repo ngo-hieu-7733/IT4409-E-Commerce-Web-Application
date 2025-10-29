91327be36b94d9aea66b98684c9bef2a1af269a1
ngo-hieu-7733 <ngohieu815@gmail.com>
2025-10-29 19:20:20 +0700

    Set up data set for sanity

--- frontend/sanity/schemaTypes/index.ts
+++ frontend/sanity/schemaTypes/index.ts
@@ -1,5 +1,24 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { categoryType } from './categoryType'
+import { addressType } from './addressType'
+import { authorType } from './authorType'
+import { blockContentType } from './blockContentType'
+import { blogCategoryType } from './blogCategoryType'
+import { blogType } from './blogType'
+import { brandType } from './brandTypes'
+import { orderType } from './orderType'
+import { productType } from './productType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [
+    categoryType,
+    addressType,
+    authorType,
+    blockContentType,
+    blogCategoryType,
+    blogType,
+    brandType,
+    orderType,
+    productType
+  ],
 }
