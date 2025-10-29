cd3b50446d71184750614ca46202b0aab9acf629
ngo-hieu-7733 <ngohieu815@gmail.com>
2025-10-29 08:28:21 +0700

    Create Home Banner

---
+++ frontend/app/HomeBanner.tsx
@@ -0,0 +1,30 @@
+import Title from "@/components/ui/text";
+import { banner_1 } from "@/images";
+import Image from "next/image";
+import Link from "next/link";
+
+const HomeBanner = () => {
+  return (
+    <>
+      <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
+        <div className="space-y-5">
+          <Title>
+            Grab Upto 50% Off On <br />
+            Selected Headphone
+          </Title>
+          <Link href={"/shop"} className="bg-shop_btn_dark_green/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_btn_dark_green hoverEffect">Buy Now</Link>
+        </div>
+
+        <div>
+          <Image
+            src={banner_1}
+            alt="banner_1"
+            className="hidden md:inline-flex w-96"
+          ></Image>
+        </div>
+      </div>
+    </>
+  )
+};
+
+export default HomeBanner;
--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -15,7 +15,7 @@ const Header = async () => {
   console.log(user)
 
   return (
-    <header className="bg-white py-5 border-b border-b-black/30">
+    <header className="bg-white py-5">
       <Container className="flex items-center justify-between text-lightColor">
         <div className="w-auto md:w-1/3 flex items-center justify-start gap-2.5 md:gap-0">
           <MobileMenu/>
